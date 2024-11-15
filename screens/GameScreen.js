30f1af4c58a8e1c4bb15ff845f802c5fc726d1ea
Mehdi <meh.tavakoli@gmail.com>
2024-11-15 11:53:40 +0100

    StartGameScreen is done!

---
+++ screens/GameScreen.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb53f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#ddb53f",
+    padding: 12,
+  },
+});
